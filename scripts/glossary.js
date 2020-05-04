2fb487c2b047019a06dab918960df9dc89f81887
Kmek <kmekeefe@gmail.com>
2020-05-04 12:01:30 -0400

    Make 1 flippable card for the glossary

--- scripts/glossary.js
+++ scripts/glossary.js
@@ -1,5 +1,46 @@
-var terms = {
-
-}
+var terms = [{
+    name: "Credit",
+    def: "test testing asdfghj",
+}, {
+    name: "Interest",
+    def: "",
+}, {
+    name: "Annual Fee",
+    def: "",
+}, {
+    name: "Annual Percentage Rate (APR)",
+    def: "",
+}, {
+    name: "Finance Change",
+    def: "",
+}, {
+    name: "Introductory Rate (Teaser Rate)",
+    def: "",
+}, {
+    name: "Minimum Payment",
+    def: "",
+}, {
+    name: "Variable Interest",
+    def: "",
+}, {
+    name: "Grace Period",
+    def: "",
+}, {
+    name: "Schumer Box",
+    def: "",
+}, {
+    name: "Secure Credit Cards",
+    def: "",
+}, {
+    name: "FICO Score",
+    def: "",
+}, {
+    name: "Deadbeat",
+    def: "",
+}, {
+    name: "Revolver",
+    def: "",
+}]
 
 // Make a wall of square divs, make them flip over like cards (with the definition on the back)
+
